34c2b2c9642d7120572eb7c507feb670110909b9
Juan Pablo Morales <juanpablo@ararads.com>
2019-09-23 00:24:50 -0500

    Added imshow

--- plot_helpers.py
+++ plot_helpers.py
@@ -83,3 +83,15 @@ def french():
     green = np.hstack([np.zeros((22,11)), np.ones((22,11)),np.zeros((22,11))])
     red = np.hstack([np.zeros((22,11)), np.ones((22,22))])
     return np.moveaxis(np.array([red,green,blue]),0,-1)
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # 
