43262f4d7e7c8c3caef8d3d8476aacf0eaf310bd
Juan Pablo Morales <juanpablo@ararads.com>
2019-09-26 21:56:00 -0500

    Added utils

---
+++ utils.py
@@ -0,0 +1,73 @@
+from torch.autograd import Variable
+import time
+import datetime
+
+# Replay buffer stores previous results from generator, so that discriminator does not forget previous generated images
+class ReplayBuffer():
+    def __init__(self, max_size=50):
+        assert (max_size > 0), 'Empty buffer or trying to create a black hole. Be careful.'
+        self.max_size = max_size
+        self.data = []
+
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                if random.uniform(0,1) > 0.5:
+                    i = random.randint(0, self.max_size-1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    to_return.append(element)
+        return Variable(torch.cat(to_return))
+
+class Logger():
+    def __init__(self, n_epochs, batches_epoch, epoch=1):
+        self.n_epochs = n_epochs
+        self.batches_epoch = batches_epoch
+        self.epoch = epoch
+        self.batch = 1
+        self.prev_time = time.time()
+        self.mean_period = 0
+        self.losses = {}
+        self.loss_windows = {}
+        self.image_windows = {}
+
+
+    def log(self, losses=None, images=None):
+        self.mean_period += (time.time() - self.prev_time)
+        self.prev_time = time.time()
+
+        sys.stdout.write('\rEpoch %03d/%03d [%04d/%04d] -- ' % (self.epoch, self.n_epochs, self.batch, self.batches_epoch))
+
+        for i, loss_name in enumerate(losses.keys()):
+            if loss_name not in self.losses:
+                self.losses[loss_name] = losses[loss_name].data.item()
+            else:
+                self.losses[loss_name] += losses[loss_name].data.item()
+            
+            if (i+1) == len(losses.keys()):
+                sys.stdout.write('%s: %.4f -- ' % (loss_name, self.losses[loss_name]/self.batch))
+            else:
+                sys.stdout.write('%s: %.4f | ' % (loss_name, self.losses[loss_name]/self.batch))
+
+        batches_done = self.batches_epoch*(self.epoch - 1) + self.batch
+        batches_left = self.batches_epoch*(self.n_epochs - self.epoch) + self.batches_epoch - self.batch 
+        sys.stdout.write('ETA: %s' % (datetime.timedelta(seconds=batches_left*self.mean_period/batches_done)))
+
+        # End of epoch
+        if (self.batch % self.batches_epoch) == 0:
+            # Plot losses
+            for loss_name, loss in self.losses.items():
+                # Reset losses for next epoch
+                self.losses[loss_name] = 0.0
+
+            self.epoch += 1
+            self.batch = 1
+            sys.stdout.write('\n')
+        else:
+            self.batch += 1
